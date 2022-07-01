61d347c0c766f2df851c983221fea2658f3419a7
MaryPluto <masha.pluyto@mail.ru>
2022-07-01 23:48:53 +0300

    docs: add index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 module.exports = function reverse (n) {
-  
+    let str = String(n);
+    let arr = str.split("");
+    arr.map((num) => {if(Math.sign(num) == -1){num = +num}});
+    let arrev = arr.reverse();
+    let revstr = arrev.join("");
+    let del = revstr.replace(/-/g, "");
+    return Number(del);
 }
+
